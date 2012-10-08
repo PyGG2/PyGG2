7dae7feb7c9fcb867b567e4c7278cf009b19c12d
Orpheon <alex.liebendorf@gmail.com>
2012-10-08 18:52:35 +0200

    Fixed the collision code. Apparently whoever designed it didn't think we'd ever have negative frametimes, but we do. Also removed the odd semicolon etc.

--- engine/character.py
+++ engine/character.py
@@ -127,21 +127,23 @@ class Character(entity.MovingObject):
         self.x += self.hspeed * frametime
         # if we are in a wall now, we must move back
 
-        if game.map.collision_mask.overlap(self.collision_mask, (int(self.x), int(self.y))):
+        if game.map.collision_mask.overlap(self.collision_mask, (int(round(self.x)), int(round(self.y)))):
             # but if we just walked onto a one-unit wall it's ok
             # but we had to be on the ground
-            if onground and not game.map.collision_mask.overlap(self.collision_mask, (int(self.x), int(self.y - 6))):
-                while game.map.collision_mask.overlap(self.collision_mask, (int(self.x), int(self.y))):
+            if onground and not game.map.collision_mask.overlap(self.collision_mask, (int(round(self.x)), int(self.y - 6))):
+                while game.map.collision_mask.overlap(self.collision_mask, (int(round(self.x)), int(round(self.y)))):
                     self.y -= 1
             # but sometimes we are so fast we will need to take two stairs at the same time
-            elif onground and not game.map.collision_mask.overlap(self.collision_mask, (int(self.x), int(self.y - 12))) and game.map.collision_mask.overlap(self.collision_mask, (int(self.x - 6 * function.sign(self.hspeed)), int(self.y))):
-                while game.map.collision_mask.overlap(self.collision_mask, (int(self.x), int(self.y))):
+            elif onground and not game.map.collision_mask.overlap(self.collision_mask, (int(round(self.x)), int(self.y - 12))) and game.map.collision_mask.overlap(self.collision_mask, (int(self.x - 6 * function.sign(self.hspeed)), int(round(self.y)))):
+                while game.map.collision_mask.overlap(self.collision_mask, (int(round(self.x)), int(round(self.y)))):
                     self.y -= 1
             else:
-                self.x = math.floor(self.x) # move back to a whole pixel - TODO math.floor/math.ceil depending on direction
-
+                if self.hspeed < 0:
+                    self.x = math.floor(self.x) # move back to a whole pixel
+                else:
+                    self.x = math.ceil(self.x)
                 # and if one pixel wasn't enough
-                while game.map.collision_mask.overlap(self.collision_mask, (int(self.x), int(self.y))):
+                while game.map.collision_mask.overlap(self.collision_mask, (int(round(self.x)), int(round(self.y)))):
                     self.x -= function.sign(self.hspeed)
 
                 self.hspeed = 0
@@ -149,20 +151,20 @@ class Character(entity.MovingObject):
         # same stuff, but now vertically
         self.y += self.vspeed * frametime
 
-        if game.map.collision_mask.overlap(self.collision_mask, (int(self.x), int(self.y))):
-            self.y = float(int(self.y))
+        if game.map.collision_mask.overlap(self.collision_mask, (int(round(self.x)), int(round(self.y)))):
+            self.y = float(round(self.y))
 
-            while game.map.collision_mask.overlap(self.collision_mask, (int(self.x), int(self.y))):
+            while game.map.collision_mask.overlap(self.collision_mask, (int(round(self.x)), int(round(self.y)))):
                 self.y -= function.sign(self.vspeed)
 
             self.vspeed = 0
 
-        player.last_left = player.left;
-        player.last_right = player.right;
+        player.last_left = player.left
+        player.last_right = player.right
 
     def onground(self, game, state):
         # are we on the ground? About one third of an unit from the ground is enough to qualify for this
-        return game.map.collision_mask.overlap(self.collision_mask, (int(self.x), int(self.y + 1)))
+        return game.map.collision_mask.overlap(self.collision_mask, (int(round(self.x)), int(self.y + 1)))
 
     def interpolate(self, prev_obj, next_obj, alpha):
         super(Character, self).interpolate(prev_obj, next_obj, alpha)
